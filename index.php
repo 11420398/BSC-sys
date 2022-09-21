860d8ff46ee91db225ed18f279594e1092d1eb83
11420398 <114053281+11420398@users.noreply.github.com>
2022-09-20 23:39:23 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!--
+Author: Reality Software
+Website: http://www.realitysoftware.ca
+Note: This is a free template released under the Creative Commons Attribution 3.0 license, 
+which means you can use it in any way you want provided you keep the link to the author intact.
+-->
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title></title>
+<link href="style.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+	<!-- header -->
+    <div id="header">
+    <div id="menu">
+       <div id="menu_list"> 
+        <a href="index.php">ዋና ድህረ ገፅ</a>
+        <img src="images/splitter.gif" class="splitter" alt="" />
+        <a href="about.php">ስለ ሲስተም</a>
+        <img src="images/splitter.gif" class="splitter" alt="" />
+        <a href="service.php">አገልግሎት</a>
+        <img src="images/splitter.gif" class="splitter" alt="" />
+       
+        
+        <a href="contact.php">አድራሻ</a>
+        <img src="images/splitter.gif" class="splitter" alt="" />
+       
+        
+        
+       
+        
+        <a href="worker_result_show.php">የቢኤስሲ አፈፃፀም ምዘና ውጤት</a>
+        </div>
+    </div>
+</div>
+      <div id="logo">
+      <div id="logo_text">
+      <a href="#"><font color="#FFFFFF">የጂኦስፓሻል መረጃ እና ደህንነት ዳይሬክቶሬት የቢኤስሲ አውቶሜሽን ሲስተም</font></a>
+      <br/>
+      
+<br/>
+     
+	</div>
+    </div>
+    <!--end header -->
+    <!-- main -->
+    <div id="main">
+          <div id="sidebar">
+          	<p>
+            <form action="login.php" method="post"><table width="585" border="0" height="30"><table width="250" border="0">
+  <tr>
+    <td colspan="2"><h2>መግቢያ</h2></td>
+    </tr>
+  <tr>
+    <td width="89"><h2>መለያ ስም</h2></td>
+    <td width="145"><input type="text" name="username" id="username"/></td>
+  </tr>
+  <tr>
+    <td><h2>የይለፍ ቃል</h2></td>
+    <td><input type="password" name="password" id="password"/></td>
+  </tr>
+  <tr>
+    <td><input type="submit" value=" &nbsp;&nbsp;&nbsp;ግባ&nbsp;&nbsp;&nbsp;" /></td>
+    <td align="right"><input type="reset" value="&nbsp;&nbsp;&nbsp;ሰርዝ&nbsp;&nbsp;&nbsp;" /></td>
+  </tr>
+</table>
+</form>
+</p>
+          </div>
+          <div id="text" >
+          <table width="1016" border="0">
+  <tr>
+    <td width="727"><h1><strong></strong><strong>Welcome</strong></h1>
+                <p><strong>
+	ኢመደኤ ተቋማዊ እቅድን ለመንደፍ፤ ራእይና ስትራተጂን ከፈፃሚ አባላት የእለት ተእለት ተግባር ጋር ላመስተሳሰር አፈፃምን በተጨባጭ መረጃ ላይ በመመስረት< ለመለካትና ለማሻሻል የሚያስችል የስራ አፈፃፀም ምዘና ስርአት በመዘርጋት ቀጣይነት ያለው ተቋማዊ መማማር እውን የሚሆንበትን አግባብ ለመፍጠር የተለያዩ የስራ አፈፃፀም ምዘና አነዱ የሆነውን የባላንስድ ስኮር ካርድ አፈፃፀም ምዘና ስርአት ተመራጭ ሆኖ ተግባራዊ መሆኑን ይታወቃል፡፡
+
+ከዚህም በመነሳት የምዘና ስርአቱ የስራ ክፍሎች ወይም ቡድኖች እንዲሁም ፕሮጀክቶች ወይም ደግሞ አባላት ለተቋሙ ማበርከት የሚገባቸውን የስራ ውጤት በሚፈለገው ደረጃና በተሟላ ሁኔታ ማከናወናቸውን ለማረጋገጥ የሚያስችል በመሆኑ የኢንፎርሜሽን መረብ ደህንነት ኢጀንሲ ይህ የባላንስድ ስኮር ካርድ የስራ አፈፃፀም ውጤት ምዘና ሲስተም አዘጋጅቷል፡፡</strong></p>
+                <ul>
+              
+                    <p align="left"><strong>ስለሆነም  ይህ ሲስተም ከዚህ በታች ባሉት አላማዎች መሰረት በማድረግ ሲስተሙ ተፈፃሚነት  እንዲኖረው ያስቀምጣል፡፡</p>
+                    <ul>
+                      <li>የባላንስድ ስኮርድ ካርድን  ሁለንተናዊ ጠቀመታ እና ወደ ተቋሙ በማላመድና ተቋማዊ ተጣጣሚነትን በመፍጠር በሚዛናዊ እድገት አቅጣጫ ተቋማዊ ስኬትን ለማሳደግ፤</li>
+                      <li>የኤጀንሲው ክፍሎችና አባላት  ተቋማዊ ተልእኮን ከማሳካት አንፃር የሚሰጣቸውን የግልና የጋራ ተግባርና ሃላፊነት በመፈፀም የደረሱበትን ደረጃና ያስመዘገቡት ውጤት  ለመመዘን፤</li>
+                      <li>የአባላትን ጥንካሬና እጥረቶች  በመለየት የተሻሉትን ለማበረታታት ክፍተት የታየባቸውን ለማገዝ የሚያስችል ከአድልዎ የፀዳና ግልፅ የአስራር ስርአት ማስፈን እንዲቻል  የተቀረፀ ሲስተም ነው፡፡</li>
+                    </ul>
+        </ul></td>
+    <td width="273"><img src="images/INSA.jpg" height="500" width="300"/></td>
+  </tr>
+</table>
+
+                
+      </div>
+    </div>
+    <!-- end main -->
+    <!-- footer -->
+    <div id="footer">
+    <div id="left_footer">&copy; Copyright 2014 <b>Information Network Security Agency(INSA)</b></div>
+    <div id="right_footer">
+
+<!-- Please do not change or delete this link. Read the license! Thanks. :-) -->
+Design by <a href="http://www.realitysoftware.ca" title="Website Design"><strong>Information Network Security Agency(INSA)</strong></a>
+
+    </div>
+</div>
+    <!-- end footer -->
+
+
+</body>
+</html>
